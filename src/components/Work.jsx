1b0a36c148695b7d925f219028e67f4b6ff4194b
avalanchewebdev <108469931+avalanchewebdev@users.noreply.github.com>
2022-06-30 13:36:37 +0100

    Add files via upload

---
+++ src/components/Work.jsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import WorkImg from '../assets/workImg.jpeg';
+import realEstate from '../assets/realestate.jpg';
+import dashBoard from '../assets/dashboard.png';
+import Ecommerce from '../assets/ecommerce.png';
+import travelAdvisor from '../assets/traveladvisor.png';
+
+const Work = () => {
+  return (
+    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+        <div className='pb-8'>
+          <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>
+            Work
+          </p>
+          <p className='py-6'>// Check out some of our recent work</p>
+        </div>
+
+{/* Container */}
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+
+            {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${dashBoard})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                React JS Dashboard
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='https://shoppy-dashboard.vercel.app/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${Ecommerce})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                Ecommerce Application
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='https://phanox.vercel.app/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+            {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${travelAdvisor})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                Travel Advisor
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='https://travel-advisor.netlify.app/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Work;
