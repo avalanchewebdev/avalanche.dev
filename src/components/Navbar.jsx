80c507b1e1cc470b4f4de1a70feff5edc27dab68
avalanchewebdev <108469931+avalanchewebdev@users.noreply.github.com>
2022-06-30 14:23:55 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -101,7 +101,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              href='https://twitter.com/AvalancheDev'
             >
               Twitter <FaTwitter size={30} />
             </a>
@@ -109,7 +109,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              href='https://www.instagram.com/avalanche_dev/'
             >
               Instagram <FaInstagram size={30} />
             </a>
